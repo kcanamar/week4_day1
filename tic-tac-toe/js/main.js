2c9b1a9aecd5e4c129ce999744424acbd510be0c
Kyle Canamar <canamar.kyle@gmail.com>
2022-03-15 19:15:23 -0700

    Tic Tat Toe game begining

---
+++ tic-tac-toe/js/main.js
@@ -0,0 +1,16 @@
+console.log("JavaScript is on")
+
+/* ------------ Constants --------------- */
+
+
+/* ------------ App's State (variables) --------------- */
+
+
+/* ------------ Cached Element Referances --------------- */
+
+
+/* ------------ Event Listeners --------------- */
+
+
+/* ------------ Functions --------------- */
+
