deeb54b74b5e110237c1093a925e1aae71f8830a
Kyle Canamar <canamar.kyle@gmail.com>
2022-03-15 21:12:37 -0700

    adds title and message container along with const and variables in JS

--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -2,21 +2,38 @@ console.log("JavaScript is on")
 
 /* ------------ Constants --------------- */
 // X & O - Players
+const PLAYERS = {
+    '1': 'X',
+    '-1': 'O',
+}
 // Win conditions
+const COMBOS = [
+    [0,1,2,],
+    [3,4,5,],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 
 
 /* ------------ App's State (variables) --------------- */
 // where the users click (where to put an x or o) - make sure a square is not taken
+let board; // a data variable that stores board positions, and what they hold
 // whose turn is it
+let turn; // will be either X or O
 // when there is a winning combination or a tie - if game is ongoing
+let winner; // this can be 3 things X/O, Tie, or null
 // Bonus - timer for each players turn
 // Bonus - score for games won
 // Bonus - if the player wants to quit
 
 /* ------------ Cached Element Referances --------------- */
+// message container - h2 
 // the game board spaces
 // reset button
-// message container - h1 
 
 /* ------------ Event Listeners --------------- */
 // mouse click on the 9 squares
