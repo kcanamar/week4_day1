3eec6ab9b1653b1dbcb9d7d1e69060f657dc0d22
Kyle Canamar <canamar.kyle@gmail.com>
2022-03-15 20:28:54 -0700

    adds reset button and styling along with JS sudo code

--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -1,13 +1,21 @@
 console.log("JavaScript is on")
 
 /* ------------ Constants --------------- */
+// X & O - Players
+// Win conditions
 
 
 /* ------------ App's State (variables) --------------- */
-
+// where the users click (where to put an x or o) - make sure a square is not taken
+// whose turn is it
+// when there is a winning combination or a tie - if game is ongoing
+// Bonus - timer for each players turn
+// Bonus - score for games won
+// Bonus - if the player wants to quit
 
 /* ------------ Cached Element Referances --------------- */
-
+// the game board spaces
+// reset button
 
 /* ------------ Event Listeners --------------- */
 
